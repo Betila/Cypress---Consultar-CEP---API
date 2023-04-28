2d95b54b15b7cdb05e1a046369160186fdaf12ba
Betila <betilasantos@gmail.com>
2023-04-28 11:57:29 -0300

    Ajuste

--- cypress/integration/getCEP.js
+++ cypress/integration/getCEP.js
@@ -4,8 +4,15 @@ import 'cypress-plugin-api'
 import apis from '../fixtures/urlAPI.json'
 import * as cepValido from '../support/escolherCEP'
 import * as getDadosCEP from '../support/dadosCEP'
-import * as rioJaneiro from '../fixtures/CepRJ.json'
-import * as saoPaulo from '../fixtures/CepSP.json'
+import * as rioJaneiro from '../fixtures/CEP/CepRJ.json'
+import * as saoPaulo from '../fixtures/CEP/CepSP.json'
+import * as rioBranco from '../fixtures/CEP/CepAC.json'
+import * as alagoas from '../fixtures/CEP/CepAL.json'
+import * as amapa from '../fixtures/CEP/CepAP.json'
+import * as amazonas from '../fixtures/CEP/CepAM.json'
+import * as bahia from '../fixtures/CEP/CepBA.json'
+import * as ceara from '../fixtures/CEP/CepCE.json'
+import * as distritoFederal from '../fixtures/CEP/CepDF.json'
 
 
 
@@ -14,7 +21,6 @@ import * as saoPaulo from '../fixtures/CepSP.json'
 describe('Realizar teste de consulta por CEP', () =>{
 
     it('Deve realizar consulta de CEPs do Rio de Janeiro e comparar valor', () =>{
-
 
         let cep = cepValido.arrayCEPRioJaneiro() // vai escolher o CEP que vai usar
         let dadosCEP = getDadosCEP.dadosCEP(rioJaneiro, cep) //deve pegar a massa de dados
@@ -38,9 +44,191 @@ describe('Realizar teste de consulta por CEP', () =>{
 
     it('Deve realizar consulta de CEPs do São Paulo e comparar valor', () =>{
 
-
         let cep = cepValido.arrayCEPSaoPaulo() // vai escolher o CEP que vai usar
         let dadosCEP = getDadosCEP.dadosCEP(saoPaulo, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Acre e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPRioBranco() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(rioBranco, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs Alagoas e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPAlagoas() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(alagoas, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Amapá e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPAmapa() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(amapa, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Amazonas e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPAmazonas() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(amazonas, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Bahia e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPBahia() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(bahia, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+    it('Deve realizar consulta de CEPs do Ceará e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPCeara() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(ceara, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Ceará e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPCeara() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(ceara, cep) //deve pegar a massa de dados
+        cy.log(JSON.stringify(dadosCEP))
+        cy.log(cep)
+        cy.log(JSON.stringify(dadosCEP))
+        cy.GETApi('', apis.apiBuscarCEP+'cep',cep).then((response) =>{
+            expect(response.status).eq(200)
+            expect(response.body.address).eq(dadosCEP.address)
+            expect(response.body.number).eq(dadosCEP.number)
+            expect(response.body.city).eq(dadosCEP.city)
+            expect(response.body.longitude).eq(dadosCEP.longitude)
+            expect(response.body.state).eq(dadosCEP.state)
+            expect(response.body.id).eq(dadosCEP.id)
+            expect(response.body.latitude).eq(dadosCEP.latitude)
+            expect(response.body.neighborhood).eq(dadosCEP.neighborhood)
+            expect(response.body.blockDelivery).eq(dadosCEP.blockDelivery)
+
+        })
+
+    })
+
+    it('Deve realizar consulta de CEPs do Distrito Federal e comparar valor', () =>{
+
+        let cep = cepValido.arrayCEPDistritoFederal() // vai escolher o CEP que vai usar
+        let dadosCEP = getDadosCEP.dadosCEP(distritoFederal, cep) //deve pegar a massa de dados
         cy.log(JSON.stringify(dadosCEP))
         cy.log(cep)
         cy.log(JSON.stringify(dadosCEP))
--- cypress/support/escolherCEP.js
+++ cypress/support/escolherCEP.js
@@ -10,3 +10,45 @@ function arrayCEPSaoPaulo() {
     return saoPaulo[(Math.random()*saoPaulo.lenght) |0]
 }
 export{arrayCEPSaoPaulo}
+
+function arrayCEPRioBranco() {
+    let rioBranco=[69908650,69909400,69909360,69909740]
+    return rioBranco[(Math.random()*rioBranco.lenght) |0]
+}
+export{arrayCEPRioBranco}
+
+function arrayCEPAlagoas() {
+    let alagoas=[57015430,57015430,57081015,57082000,57300080]
+    return alagoas[(Math.random()*alagoas.lenght) |0]
+}
+export{arrayCEPAlagoas}
+
+function arrayCEPAmapa() {
+    let amapa=[68905630,68904001,68900030,68900020,68900110]
+    return amapa[(Math.random()*amapa.lenght) |0]
+}
+export{arrayCEPAmapa}
+
+function arrayCEPAmazonas() {
+    let amapa=[69151000,69088240,69075771,69047000,69005070]
+    return amapa[(Math.random()*amapa.lenght) |0]
+}
+export{arrayCEPAmazonas}
+
+function arrayCEPBahia() {
+    let bahia=[40470630,41150000,41195000,41225190,40335000]
+    return bahia[(Math.random()*bahia.lenght) |0]
+}
+export{arrayCEPBahia}
+
+function arrayCEPCeara() {
+    let ceara=[63010010,60312060,60510290,60030010,60165011]
+    return ceara[(Math.random()*ceara.lenght) |0]
+}
+export{arrayCEPCeara}
+
+function arrayCEPDistritoFederal() {
+    let distritoFederal=[73100090,73100120,70620090,72669690,72669685]
+    return distritoFederal[(Math.random()*distritoFederal.lenght) |0]
+}
+export{arrayCEPDistritoFederal}
